7bcd8d959b9b1122e16cc04ddcd753620f3eda85
mahikumarp <131871676+mahikumarp@users.noreply.github.com>
2023-04-26 16:45:52 +0530

    Update streamlit_app.py
    added multiple titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.title('Breakfast Menu')
+
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+
+streamlit.title('Hard-Boiled Free-Ranmge Egg')
