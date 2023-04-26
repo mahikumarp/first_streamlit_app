a59ec6aad2ecc25cf0621175d7f4c34d94e00311
mahikumarp <131871676+mahikumarp@users.noreply.github.com>
2023-04-26 16:56:05 +0530

    Update streamlit_app.py
    make proper text as per demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.title('Breakfast Menu')
-
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-
-streamlit.title('Hard-Boiled Free-Ranmge Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Ranmge Egg')
