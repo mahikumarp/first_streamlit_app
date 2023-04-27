0aa64bac1ec8ebb748c350c95e88e58feb23e7f7
mahikumarp <131871676+mahikumarp@users.noreply.github.com>
2023-04-27 20:57:39 +0530

    Update streamlit_app.py
    added fruit chioce drop down

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as table
 streamlit.dataframe(fruityvice_normalized)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/",+fruit_choice)
